0c62b3dd2df5c794630552089a6838525eeac9aa
archibate <1931127624@qq.com>
2024-05-07 07:15:51 +0800

    update perf

--- perf.hpp
+++ perf.hpp
@@ -1,151 +1,268 @@
 #pragma once
 
-#ifndef PERF_ENABLE
-#define PERF_ENABLE 1
-#endif
-
-#if PERF_ENABLE
-
+#include <algorithm>
 #include <chrono>
 #include <csignal>
 #include <cstdlib>
-#include <cstdio>
 #include <deque>
-#include <format>
-#include <limits>
+#include <fstream>
 #include <map>
-#include <string_view>
+#include <mutex>
 #include <vector>
-#include <source_location>
+#if __cplusplus >= 202002L
+# if defined(__has_include)
+#  if __has_include(<source_location>)
+#   include <source_location>
+#  endif
+# endif
+#endif
 
 struct Perf {
-    char const *file;
-    int line;
-    std::chrono::high_resolution_clock::time_point t0;
+    struct Location {
+        char const *func;
+        char const *file;
+        int line;
 
-    struct PerfStatic {
-        struct TableEntry {
-            std::uint64_t duration;
-            char const *file;
-            int line;
+    private:
+        struct Private {
+            explicit Private() = default;
         };
 
-        std::deque<TableEntry> table;
+    public:
+#if __cpp_lib_source_location
+        Location(std::source_location const &loc =
+                     std::source_location::current()) noexcept
+            : Location(loc.function_name(), loc.file_name(), loc.line()) {}
 
-        PerfStatic() {
-            signal(
+        Location(const char *func, std::source_location const &loc =
+                     std::source_location::current()) noexcept
+            : Location(func, loc.file_name(), loc.line()) {}
+
+# define PERF Perf()
+#else
+# if defined(__has_builtin)
+#  if __has_builtin(__builtin_FUNCTION) && __has_builtin(__builtin_FILE) && \
+      __has_builtin(__builtin_LINE)
+        Location(char const *func = __builtin_FUNCTION(),
+                 Private = Private(),
+                 char const *file = __builtin_FILE(),
+                 int line = __builtin_LINE()) noexcept
+            : Location(func, file, line) {}
+
+#   define PERF(...) Perf()
+#  else
+        Location(const char *func = "?") noexcept : Location(func, "???", 0) {}
+
+#   define PERF(...) Perf({__func__, __FILE__, __LINE__})
+#  endif
+# else
+        Location(const char *func = "?") noexcept : Location(func, "???", 0) {}
+
+#  define PERF(...) Perf({__func__, __FILE__, __LINE__})
+# endif
+#endif
+
+        Location(char const *func, char const *file, int line) noexcept
+            : func(func),
+              file(file),
+              line(line) {}
+
+        bool operator<(Location const &rhs) const {
+            return func < rhs.func ||
+                   (func == rhs.func &&
+                    (file < rhs.file || (file == rhs.file && line < rhs.line)));
+        }
+    };
+
+private:
+    Location location;
+    std::uint64_t t0;
+
+    struct PerfTableEntry {
+        std::uint64_t t0;
+        std::uint64_t t1;
+        Location location;
+
+        PerfTableEntry(std::uint64_t t0, std::uint64_t t1, Location location)
+            : t0(t0),
+              t1(t1),
+              location(location) {}
+    };
+
+    struct PerfThreadLocal {
+        std::deque<PerfTableEntry> table;
+
+        PerfThreadLocal() = default;
+
+        PerfThreadLocal(PerfThreadLocal &&) = delete;
+
+        ~PerfThreadLocal() {
+            gatherData(table);
+        }
+    };
+
+#if __cpp_inline_variables
+    static inline thread_local PerfThreadLocal perthread;
+#else
+    static PerfThreadLocal &perthread() {
+        static thread_local PerfThreadLocal perthread;
+        return perthread;
+    }
+#endif
+
+    struct PerfGather {
+        PerfGather() {
+            output = std::getenv("PERF_OUTPUT");
+            std::signal(
                 SIGINT, +[](int signo) { std::exit(130); });
         }
 
-        PerfStatic(PerfStatic &&) = delete;
+        PerfGather(PerfGather &&) = delete;
 
-        ~PerfStatic() {
-            dump();
-        }
-
-        void dump() const {
-            if (table.empty()) {
-                return;
+        ~PerfGather() {
+            if (output) {
+                std::ofstream fout(output);
+                for (auto const &entry: table) {
+                    fout << entry.t0 << ',' << entry.t1 << ','
+                         << entry.location.func << ',' << entry.location.file
+                         << entry.location.line;
+                }
             }
 
-            struct PairLess {
-                bool
-                operator()(std::pair<std::string_view, int> const &a,
-                           std::pair<std::string_view, int> const &b) const {
-                    return std::tie(a.first, a.second) <
-                           std::tie(b.first, b.second);
-                }
-            };
-
-            struct Entry {
+            struct ChartEntry {
                 std::uint64_t min = std::numeric_limits<std::uint64_t>::max();
                 std::uint64_t sum = 0;
                 std::uint64_t max = 0;
                 std::uint64_t nr = 0;
 
-                Entry &operator+=(std::uint64_t d) {
+                void add(PerfTableEntry const &e) {
+                    std::uint64_t d = e.t1 - e.t0;
                     min = std::min(min, d);
                     sum += d;
                     max = std::max(max, d);
                     ++nr;
-                    return *this;
                 }
             };
 
-            std::map<std::pair<std::string_view, int>, Entry, PairLess> m;
-            for (auto const &e: table) {
-                m[{e.file, e.line}] += e.duration;
+            std::map<Location, ChartEntry> chart;
+            for (auto it = table.begin(); it != table.end(); ++it) {
+                chart[it->location].add(*it);
             }
 
-            auto t = [](long d) -> std::string {
+            auto timeFormat = [](std::uint64_t d) -> std::string {
                 if (d < 10000) {
-                    return std::format("{}ns", d);
+                    return std::to_string(d) + "ns";
                 } else if (d < 10000000) {
-                    return std::format("{}us", d / 1000);
+                    return std::to_string(d / 1000) + "us";
                 } else if (d < 10000000000) {
-                    return std::format("{}ms", d / 1000000);
+                    return std::to_string(d / 1000000) + "ms";
                 } else if (d < 10000000000000) {
-                    return std::format("{}s", d / 1000000000);
+                    return std::to_string(d / 1000000000) + "s";
                 } else {
-                    return std::format("{}h", d / 3600000000000);
+                    return std::to_string(d / 3600000000000) + "h";
                 }
             };
 
-            auto p = [](std::string_view s) -> std::string {
+            auto simplifyPath = [](std::string s) -> std::string {
                 auto p = s.rfind('/');
-                if (p == std::string_view::npos) {
-                    return std::string(s);
+                if (p == std::string::npos) {
+                    return s;
                 } else {
-                    return std::string(s.substr(p + 1));
+                    return s.substr(p + 1);
                 }
             };
 
-            std::vector<std::pair<std::pair<std::string_view, int>, Entry>>
-                sorted(m.begin(), m.end());
-            std::sort(sorted.begin(), sorted.end(),
-                      [](auto const &lhs, auto const &rhs) {
-                          return lhs.second.sum > rhs.second.sum;
-                      });
+            auto simplifyFunc = [](std::string s) -> std::string {
+                auto p = s.rfind('/');
+                if (p == std::string::npos) {
+                    return s;
+                } else {
+                    return s.substr(p + 1);
+                }
+            };
 
-            std::size_t w = 0, nw = 1;
-            for (auto const &[loc, e]: sorted) {
-                w = std::max(w, p(loc.first).size());
-                nw = std::max(nw, std::to_string(e.nr).size());
+            struct ChartCompare {
+                bool operator()(std::pair<Location, ChartEntry> const &a,
+                                std::pair<Location, ChartEntry> const &b) {
+                    return a.second.sum > b.second.sum;
+                }
+            };
+
+            std::vector<std::pair<Location, ChartEntry>> sorted(chart.begin(),
+                                                                chart.end());
+            std::sort(sorted.begin(), sorted.end(), ChartCompare());
+
+            std::size_t funcWidth = 0, pathWidth = 0, nrWidth = 1;
+            for (auto it = sorted.begin(); it != sorted.end(); ++it) {
+                funcWidth =
+                    std::max(funcWidth, simplifyFunc(it->first.func).size());
+                pathWidth =
+                    std::max(pathWidth, simplifyPath(it->first.file).size());
+                nrWidth =
+                    std::max(nrWidth, std::to_string(it->second.nr).size());
             }
 
-            std::string o;
-            auto oit = std::back_inserter(o);
-            std::format_to(oit, "{:>{}}:{:<4} {:^6} {:^6} {:^6} {:^{}}\n",
-                           "file", w, "line", "min", "avg", "max", "nr",
-                           nw + 1);
-            for (auto const &[loc, e]: sorted) {
-                std::format_to(oit, "{:>{}}:{:<4} {:>6} {:>6} {:>6} {:>{}}x\n",
-                               p(loc.first), w, loc.second, t(e.min),
-                               t(e.sum / e.nr), t(e.max), e.nr, nw);
+            std::printf("==== PERF REPORT ====\n");
+            std::printf("%*s %*s:%-4s %6s %6s %6s %*s\n", (int)funcWidth,
+                        "func", (int)pathWidth, "file", "line", "min", "avg",
+                        "max", (int)nrWidth + 1, "nr");
+            for (auto it = sorted.begin(); it != sorted.end(); ++it) {
+                std::printf("%*s %*s:%-4d %6s %6s %6s %*lux\n", (int)funcWidth,
+                            simplifyPath(it->first.func).c_str(),
+                            (int)pathWidth,
+                            simplifyPath(it->first.file).c_str(),
+                            it->first.line, timeFormat(it->second.min).c_str(),
+                            timeFormat(it->second.sum / it->second.nr).c_str(),
+                            timeFormat(it->second.max).c_str(), (int)nrWidth,
+                            it->second.nr);
             }
-            printf("%s", o.c_str());
         }
+
+        std::deque<PerfTableEntry> table;
+        std::mutex lock;
+        char const *output;
     };
 
-    static inline thread_local PerfStatic stat;
+#if __cpp_inline_variables
+    static inline PerfGather gathered;
+
+    static void gatherData(std::deque<PerfTableEntry> const &table) {
+        std::lock_guard<std::mutex> guard(gathered.lock);
+        gathered.table.insert(gathered.table.end(), table.begin(), table.end());
+    }
+#else
+    static PerfGather &gathered() {
+        static PerfGather gathered;
+        return gathered;
+    }
+
+    static void gatherData(std::deque<PerfTableEntry> const &table) {
+        PerfGather &g = gathered();
+        std::lock_guard<std::mutex> guard(g.lock);
+        g.table.insert(g.table.end(), table.begin(), table.end());
+    }
+#endif
 
 public:
-    Perf(std::source_location loc = std::source_location::current())
-        : file(loc.file_name()),
-          line(loc.line()),
-          t0(std::chrono::high_resolution_clock::now()) {}
+    Perf(Location location = Location())
+        : location(location),
+          t0(std::chrono::high_resolution_clock::now()
+                 .time_since_epoch()
+                 .count()) {}
+
+    Perf(const char *func)
+        : Perf(Location(func)) {}
 
     Perf(Perf &&) = delete;
 
     ~Perf() {
-        auto t1 = std::chrono::high_resolution_clock::now();
-        auto duration = (t1 - t0).count();
-        stat.table.emplace_back(duration, file, line);
+        auto t1 = std::chrono::high_resolution_clock::now()
+                      .time_since_epoch()
+                      .count();
+#if __cpp_inline_variables
+        perthread.table.emplace_back(t0, t1, location);
+#else
+        perthread().table.emplace_back(t0, t1, location);
+#endif
     }
 };
-
-#else
-
-struct Perf {};
-
-#endif
